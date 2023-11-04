ea154c2cc1b29a866f369ae55646067f4e0de795
Samiiahmed <eldon.smsm135@gmail.com>
2023-11-04 12:28:45 +0200

    Initial Commit

---
+++ N.M.C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  N.M.C
+//
+//  Created by Sami Ahmed on 04/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
